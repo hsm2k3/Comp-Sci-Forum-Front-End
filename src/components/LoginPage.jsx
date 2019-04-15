db4ccb58ad4c901b93640fa55875934e079c4100
Mike Bajor <michael.bajor@protonmail.com>
2019-04-15 11:33:48 -0400

    Add LoginPage component

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
+import { Form, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 class LoginPage extends Component {
     render(){
         return(
             <div id={"LoginPage"}>
-                <h1>Login Page</h1>
+                <Form>
+                    <h2>Login</h2>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="dark">
+                        Login
+                    </Button>
+                </Form>
             </div>
         );
     };
