8b52ce89de8da288526206bc7f8e106bcfb37995
mbajor14 <michael.bajor@protonmail.com>
2019-04-15 07:28:33 -0400

    Edit Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoePrints } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
 const Footer = () => {
     return(
         <footer id={"Footer"}>
+            <FontAwesomeIcon icon={faShoePrints}/>
             Footer
+            <FontAwesomeIcon icon={faShoePrints}/>
         </footer>
     );
 }
