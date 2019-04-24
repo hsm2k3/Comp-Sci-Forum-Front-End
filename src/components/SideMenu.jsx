7b5507da041d6a4b450820e062b364e871c203b5
mbajor14 <michael.bajor@protonmail.com>
2019-04-24 18:35:45 -0400

    Added componentDidMount to SideMenu component

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+
 class SideMenu extends Component {
     constructor(props){
         super(props);
         this.state = {  };
+    }
+
+    componentDidMount(){
+        fetch('localhost:3001/api/sections')
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch(() => console.log('Something went wrong!'))
     }
 
     render(){
