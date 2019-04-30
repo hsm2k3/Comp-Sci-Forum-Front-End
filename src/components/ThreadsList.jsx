ba0674e5f4f840a4c5009449a8e51d7a944d5b02
mbajor14 <michael.bajor@protonmail.com>
2019-04-29 23:16:56 -0400

    Added ThreadList component

---
+++ src/components/ThreadsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const ThreadsList = props => (
+    <div id={'ThreadsList'}>
+        <h1>Threads for {props.sectionName} go here!</h1>
+    </div>
+);
+
+
+export default ThreadsList;
