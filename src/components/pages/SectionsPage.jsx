bc08e2d5f94e04edbe3195d537e62455c4cdc44c
mbajor14 <michael.bajor@protonmail.com>
2019-04-29 22:13:15 -0400

    Add render section data when routing to page

--- src/components/pages/SectionsPage.jsx
+++ src/components/pages/SectionsPage.jsx
@@ -4,15 +4,46 @@ import React, {Component} from 'react';
 class SectionsPage extends Component {
     constructor(props){
         super(props);
-        this.state = {  };
+        this.state = {
+            section: null
+        };
     }
 
+    componentDidMount() {
+        this.fetchSectionData();
+    }
+
+    componentDidUpdate(prevProps) {
+        // Typical usage (don't forget to compare props):
+        if (this.props.match.params.section !== prevProps.match.params.section) {
+            this.fetchSectionData();
+        }
+    }
+
+    fetchSectionData = () => {
+        fetch(`/api/sections/code/${this.props.match.params.section}`)
+            .then( res => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({ section: data});
+            })
+            .catch(() => console.error('SectionPage: unable to fetch data'))
+    };
+
     render(){
-        const {match} = this.props;
+        const { section } = this.state;
 
         return(
             <div id={"SectionsPage"}>
-                <h1>Sections Page: {match.params.section}</h1>
+                {
+                    section &&
+                        <div className={'SectionsPageHeader'}>
+                            <h1>{section.code}</h1>
+                            <h3>{section.title}</h3>
+                            <p>{section.description}</p>
+                        </div>
+                }
             </div>
         );
     };
