10755cc9687fe38735c604e2d63afe6d81f137b8
Mike Bajor <michael.bajor@protonmail.com>
2019-05-02 21:38:41 -0400

    ThreadList style and structure  updated

--- src/components/ThreadsList.jsx
+++ src/components/ThreadsList.jsx
@@ -4,7 +4,17 @@ import React from 'react';
 const ThreadsList = props => (
     props.threads.map( thread => {
         return <li className={'ThreadsListItem'} key={thread.id} >
-            <h3>{thread.title}</h3>
+            <div className={"ThreadsListItemHead"}>
+                <div className={"ThreadsListItemTitle"}>
+                    <h5>{ thread.title }</h5>
+                </div>
+                <div className={"ThreadsListItemAuthor"}>
+                    <p>Posted by {thread.user_id}</p>
+                </div>
+            </div>
+            <div className={"ThreadsListItemContent"}>
+                <p>{ thread.content }</p>
+            </div>
         </li>
     })
 );
