5e2ce5c41fd0200e2ac1db4b35cc2f0532bc7ccd
Mike Bajor <michael.bajor@protonmail.com>
2019-05-03 01:40:48 -0400

    Changed h3 to h4 in SectionsPage header

--- src/components/pages/SectionsPage.jsx
+++ src/components/pages/SectionsPage.jsx
@@ -89,7 +89,7 @@ class SectionsPage extends Component {
                     section &&
                         <Fragment>
                             <div className={'SectionsPageHeader'}>
-                                <h3>{section.code}: {section.title}</h3>
+                                <h4>{section.code}: {section.title}</h4>
                                 <p>{section.description}</p>
                             </div>
                             {console.log('before', section.Threads)}
