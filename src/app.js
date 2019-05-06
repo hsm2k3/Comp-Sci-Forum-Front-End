0b42a90e8e0f59b04b0e97dd7f027e114d1ba177
Mike Bajor <michael.bajor@protonmail.com>
2019-05-06 17:26:04 -0400

    Configuring for passport

--- src/app.js
+++ src/app.js
@@ -15,7 +15,27 @@ import ThreadsPage from './components/pages/ThreadsPage';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = { }
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        fetch("/api/auth")
+            .then(response => {
+                console.log(response);
+                return response.json();
+            })
+            .then(data => {
+                this.setState({user: data, isLoading: false});
+                console.log(data);
+            })
+            .catch(error => {
+                setTimeout(() => {
+                    this.setState({isLoading: false})
+                }, 1000);
+            }
+        )
     }
 
     render() {
@@ -32,7 +52,7 @@ class App extends Component {
                               <Route path={"/about"} component={AboutPage}/>
                               <Route path={"/profile"} component={ProfilePage}/>
                               <Route path={"/login"} component={LoginPage}/>
-                              <Route path={"/sections/:section"} component={SectionsPage}/>
+                              <Route path={"/sections/:section"} component={SectionsPage} exact={true}/>
                               <Route path={"/sections/:section/:thread"} component={ThreadsPage}/>
                               <Redirect to={"/"}/>
                           </Switch>
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -3,8 +3,57 @@ import { Form, Button } from 'react-bootstrap';
 // import PropTypes from 'prop-types';
 
 class LoginPage extends Component {
-    handleSubmit = (event) => {
-        console.log('handleSubmit ran', event);
+    constructor(props){
+        super(props);
+        this.state = {
+            email: null,
+            password: null
+        }
+    }
+
+
+    handleSubmit = event => {
+        event.preventDefault();
+        fetch("/login", {
+            method: "post",
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+            .then(res => {
+                console.log("handleSubmit",res);
+                return res.json();
+            })
+            .then(user => {
+                console.log("%cLogin-user-debug", "color: purple");
+                console.log({user});
+                if(user){
+                    // this.props.getUser(user);
+                    this.props.history.push("/profile");
+                }
+            })
+            .catch(error => {
+                this.setState({exists: true})
+
+            });
+    };
+
+    handleEmailChange = event => {
+        this.setState({
+          email: event.target.value
+        });
+        console.log(event.target.value);
+    };
+
+    handlePasswordChange = event => {
+        this.setState({
+            password: event.target.value
+        });
+        console.log(event.target.value);
     };
 
     render(){
@@ -14,7 +63,7 @@ class LoginPage extends Component {
                     <h2>Login</h2>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" onChange={this.handleEmailChange}/>
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -22,7 +71,7 @@ class LoginPage extends Component {
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={this.handlePasswordChange}/>
                     </Form.Group>
                     <Button variant="dark" type="submit">
                         Login
@@ -30,7 +79,7 @@ class LoginPage extends Component {
                 </Form>
             </div>
         );
-    };
+    }
 }
 
 // LoginPage.propTypes = {
