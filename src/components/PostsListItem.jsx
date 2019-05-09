22b6430c3c95da6af2e872cdfd8c458dee0b0203
Mike Bajor <michael.bajor@protonmail.com>
2019-05-09 13:28:54 -0400

    Styling PostsListItem

--- src/components/PostsListItem.jsx
+++ src/components/PostsListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PostsListItem = props => (
     <li className={"PostsListItem"} key={props.id}>
-        <div className={'PostListItemContent'}>
+        <div className={'PostsListItemContent'}>
             <p>{props.content}</p>
         </div>
     </li>
