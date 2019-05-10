8e89968f4699bad137418dbd098852ebb3b2538c
Mike Bajor <michael.bajor@protonmail.com>
2019-05-10 19:44:39 -0400

    code maid visited and cleaned some sytax

--- src/components/ThreadsList.jsx
+++ src/components/ThreadsList.jsx
@@ -5,7 +5,8 @@ import ThreadsListItem from './ThreadsListItem';
 
 const ThreadsList = props => (
     props.threads.map( thread => {
-        return <NavLink className={"ThreadsListItem-Link"} key={thread.id} to={`/sections/${props.section.code ? props.section.code : props.section.title}/${thread.id}`}>
+        return <NavLink className={"ThreadsListItem-Link"} key={thread.id}
+                        to={`/sections/${props.section.code ? props.section.code : props.section.title}/${thread.id}`}>
             <ThreadsListItem title={thread.title} user_id={thread.user_id} content={thread.content}/>
     </NavLink>
     })
