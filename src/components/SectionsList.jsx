f9bafe7505249d438514f1493aec14003b9afddb
mbajor14 <michael.bajor@protonmail.com>
2019-07-11 01:44:36 -0400

    Changed syntax of sectionsList_enterMessage

--- src/components/SectionsList.jsx
+++ src/components/SectionsList.jsx
@@ -6,9 +6,9 @@ const SectionsList = props => (
     props.sections.map(section => {
         return <NavLink className={"SectionListLink"} key={section.id} to={`/sections/${section.code ? section.code : section.title}`}>
             <li className={"SectionListItem"} >
-                { props.oneResult && <span className="SectionsList_enterMessage">----ENTER--( </span> }
+                { props.oneResult && <span className="SectionsList_enterMessage">ENTER--->[ </span> }
                 { section.code ? section.code : section.title }
-                { props.oneResult && <span className="SectionsList_enterMessage"> )----</span> }
+                { props.oneResult && <span className="SectionsList_enterMessage"> ]</span> }
             </li>
         </NavLink>
     })
